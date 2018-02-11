d43e1d11eaaaf521bd25a4b7c176a50d705dce43
Lorenc326 <batcoh@ukr.net>
2018-02-12 00:16:27 +0200

    hw_07 Add first task

---
+++ homework_07/js/task1.js
@@ -0,0 +1,22 @@
+let prom = prompt('Enter natural number: ', '');
+let n = Number(prom);
+let pyramid = '';
+
+if(!isNaN(parseFloat(prom)) && isFinite(prom) && (n <= 20) && (n > 0)){
+
+    for(let row = 0; row < n; row++){
+        for(let space = n - row; space > 1; space--){
+            pyramid += `   `;
+        }
+        for(let brick = 0; brick <= 2*row; brick++){
+            pyramid += '[~]';
+        }
+        for(let space = n - row; space > 1; space--){
+            pyramid += `   `;
+        }
+        pyramid += '\n'
+    }
+    console.log(pyramid);
+} else{
+    console.error('Incorrect!!!');
+}
