cee576557df115f87d9065a9594a9d123617f63f
Lorenc326 <batcoh@ukr.net>
2018-02-24 19:55:43 +0200

    hw_10 Add example in first task

--- homework_10/task1.js
+++ homework_10/task1.js
@@ -7,3 +7,29 @@ function debounce(func, delay){
         timer = setTimeout(func, delay);
     };
 }
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+    iterator++;
+
+    printIteratorValue();
+}
+
+function printIteratorValue() {
+    console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
