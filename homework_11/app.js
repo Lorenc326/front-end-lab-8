193e7c48c187bb1a604c6f857eee59e94ef2abcb
Lorenc326 <batcoh@ukr.net>
2018-02-25 18:10:56 +0200

    hw_11 Add onclick function and css

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,24 +1,40 @@
 let rootNode = document.getElementById("root");
 
-// Your code goes here
 function createTree(options){
     let ul = document.createElement('ul');
     for(let i = 0; i < options.length; i++){
         let li = document.createElement('li');
         if(options[i].folder){
-            li.innerHTML = `<i class="material-icons">folder</i>   ${options[i].title}`;
+            li.innerHTML = `<div><i class="material-icons orange">folder</i>   <span>${options[i].title}</span></div>`;
+            li.classList.add('folder');
             if(options[i].children){
                 let childUl = createTree(options[i].children);
                 li.appendChild(childUl);
             } else{
-                li.innerHTML += "<ul><li>Folder is empty</li></ul>";
+                li.innerHTML += "<ul><i>Folder is empty</i></ul>";
             }
         } else {
-            li.innerHTML = `<i class="material-icons">insert_drive_file</i>   ${options[i].title}`;
+            li.innerHTML = `<div><i class="material-icons grey">insert_drive_file</i>   <span>${options[i].title}</span></div>`;
         }
         ul.appendChild(li);
     }
     return ul;
+}
+
+rootNode.onclick = function (event){
+    let target = event.target;  // make onclick function by declaration
+    while(target !== this){
+        if((target.tagName === 'LI') && target.classList.contains('folder')){
+            if(target.firstChild.firstChild.innerHTML==='folder'){  // change pic of folder
+                target.firstChild.firstChild.innerHTML='folder_open';
+            } else{
+                target.firstChild.firstChild.innerHTML='folder';
+            }
+            target.classList.toggle('folOpened')
+            return;
+        }
+        target = target.parentNode;
+    }
 }
 
 rootNode.appendChild(createTree(structure));
