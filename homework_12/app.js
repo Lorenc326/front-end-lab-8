ba7727367254317ce01b7c50dcf084549ffbfc98
Lorenc326 <batcoh@ukr.net>
2018-03-03 16:05:35 +0200

    Add detailsPage and css, finish

--- homework_12/app.js
+++ homework_12/app.js
@@ -1,7 +1,6 @@
 let root = document.getElementById('root');
 
-
-function defaultPage(){
+function defaultPage(root){
     // refresh
     window.location.hash = '';
     root.innerHTML = '';
@@ -18,6 +17,7 @@ function defaultPage(){
 
     for(let i = 0; i < tanks.length; i++){
         let item = document.createElement('div');
+        item.classList.add('item');
         item.innerHTML = `<img class="tank" src="${tanks[i].preview}">
 <img class="flag" src="${tanks[i].country_image}">
 <span class="model" title="${tanks[i].model}">${tanks[i].model}</span>`
@@ -26,8 +26,55 @@ function defaultPage(){
         };
         list.appendChild(item);
     }
-
-    return;
 }
 
-defaultPage();
+function detailsPage(root, tank) {
+    root.innerHTML = `<div class="tank-details">
+    <h1><img class="detailsFlag" src="${tank.country_image}"/> ${tank.model.toUpperCase()} (level ${tank.level})</h1>
+    <div class="preview">
+        <div class="heading">Preview</div>
+        <img class="detailsTank" src="${tank.preview}">
+        <span id="backLink">Back to list view</span>
+    </div>
+    <div class="characteristic">
+        <div class="heading">Characteristic</div>
+        <table>
+            <tr>
+                <th> damage </th>
+                <th> ${tank.details.damage} </th>
+            </tr>
+            <tr>
+                <th> breoning </th> 
+                <th> ${tank.details.breoning} </th> 
+            </tr> 
+            <tr> 
+                <th> attack speed </th> 
+                <th> ${tank.details.attack_speed} </th> 
+            </tr> 
+            <tr> 
+                <th> time of targeting </th> 
+                <th> ${tank.details.time_of_targeting} </th> 
+            </tr> 
+            <tr> 
+                <th> ammunition </th> 
+                <th> ${tank.details.ammunition} </th> 
+            </tr> 
+        </table> 
+    </div> 
+</div>`;
+    let link = document.getElementById('backLink');
+    link.onclick = function () {
+        defaultPage(root);
+    }
+}
+
+window.onhashchange = function () {
+    for(let i = 0; i < tanks.length; i++){
+        if(location.hash.slice(1)===tanks[i].model){
+            detailsPage(root, tanks[i]);
+            return;
+        }
+    }
+}
+
+defaultPage(root);
